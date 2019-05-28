3b010aa335aa92ae541fa9460b7f3a22b96f16c5
Nicole Priddey <nicolepriddey@gmail.com>
2019-05-28 20:10:36 +1200

    Trying to get the application to connect to mongodb

--- BookingSystem/src/main/java/bookingSys/Connect.java
+++ BookingSystem/src/main/java/bookingSys/Connect.java
@@ -1,9 +1,7 @@
 package bookingSys;
 
-import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
@@ -11,15 +9,34 @@ import com.mongodb.MongoClientURI;
 public class Connect {	
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+	
+		
 		
 		try {
-			MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://nicole:M@ngoDb1@priddey-8mqsj.mongodb.net/test?retryWrites=true"));
-			DB database = mongoClient.getDB("sample_mflix");
-			DBCollection collection = database.getCollection("movies");
-			DBObject query = new BasicDBObject("plot", "Three men hammer on an anvil and pass a bottle of beer around.");
-			DBCursor cursor = collection.find(query);
-			DBObject movie = cursor.one();
-			System.out.println((String)cursor.one().get("title"));	
+			
+
+			MongoClientURI uri = new MongoClientURI("mongodb://nicole:M@ngoDb1@priddey-8mqsj.mongodb.net/DaySpa");
+			MongoClient mongoClient = new MongoClient(uri);
+			DB database = mongoClient.getDB("DaySpa");
+			DBCollection collection = database.getCollection("Staff");
+			DBObject myDoc = collection.findOne();
+			System.out.println(myDoc);
+			System.out.println("Hello World");
+
+//			DBCollection collection = database.getCollection("movies");
+//			DBObject query = new BasicDBObject("plot", "Three men hammer on an anvil and pass a bottle of beer around.");
+//			DBCursor cursor = collection.find(query);
+//			DBObject movie = cursor.one();
+//			System.out.println((String)cursor.one().get("title"));	
+//					
+			
+			//MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://nicole:M@ngoDb1@priddey-8mqsj.mongodb.net/test?retryWrites=true"));
+//			DB database = mongoClient.getDB("sample_mflix");
+//			DBCollection collection = database.getCollection("movies");
+//			DBObject query = new BasicDBObject("plot", "Three men hammer on an anvil and pass a bottle of beer around.");
+//			DBCursor cursor = collection.find(query);
+//			DBObject movie = cursor.one();
+//			System.out.println((String)cursor.one().get("title"));	
 		} catch (Exception e) {
 			System.out.println(e);
 		}
