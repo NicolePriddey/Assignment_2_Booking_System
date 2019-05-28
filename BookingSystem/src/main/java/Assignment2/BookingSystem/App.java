34978b839773973cb5d1f3091f1b44d5ae86f991
Nicole Priddey <nicolepriddey@gmail.com>
2019-05-28 13:49:57 +1200

    Connection attempt
    connection url not working

---
+++ BookingSystem/src/main/java/Assignment2/BookingSystem/App.java
@@ -0,0 +1,14 @@
+package Assignment2.BookingSystem;
+
+
+
+
+public class App 
+{
+	
+    public static void main( String[] args )
+    {
+    	
+    	System.out.println( "Hello World!" );
+    }
+}
---
+++ BookingSystem/src/main/java/bookingSys/Connect.java
@@ -0,0 +1,28 @@
+package bookingSys;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+public class Connect {	
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		
+		try {
+			MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb+srv://nicole:M@ngoDb1@priddey-8mqsj.mongodb.net/test?retryWrites=true"));
+			DB database = mongoClient.getDB("sample_mflix");
+			DBCollection collection = database.getCollection("movies");
+			DBObject query = new BasicDBObject("plot", "Three men hammer on an anvil and pass a bottle of beer around.");
+			DBCursor cursor = collection.find(query);
+			DBObject movie = cursor.one();
+			System.out.println((String)cursor.one().get("title"));	
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
+
