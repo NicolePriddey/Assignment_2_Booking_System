b12bea46b4c04438d2db279ca6ab67ca2aea15a3
Nicole Priddey <nicolepriddey@gmail.com>
2019-06-02 22:01:28 +1200

    layout of client gui

--- BookingGUI/src/gui/ServerGui.java
+++ BookingGUI/src/gui/ServerGui.java
@@ -14,11 +14,15 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Combo;
 
 public class ServerGui {
 
 	protected Shell shell;
 	static Registry registry;
+	private Table table;
 	/**
 	 * Launch the application.
 	 * @param args
@@ -61,41 +65,42 @@ public class ServerGui {
 	 */
 	protected void createContents() {
 		shell = new Shell();
-		shell.setSize(450, 300);
+		shell.setSize(785, 529);
 		shell.setText("SWT Application");
 		
-		Button btnStart = new Button(shell, SWT.NONE);
-		btnStart.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				try {
-					registry = LocateRegistry.createRegistry(1099);
-					registry.rebind("passkey", new Servant());
-					System.out.println("Server is ready");
-				} catch (RemoteException e1) {
-					e1.printStackTrace();
-				}
-
-			}
-		});
-		btnStart.setBounds(37, 45, 85, 26);
-		btnStart.setText("Start");
+		table = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION);
+		table.setLinesVisible(true);
+		table.setHeaderVisible(true);
+		table.setBounds(38, 130, 704, 217);
 		
-		Button btnStop = new Button(shell, SWT.NONE);
-		btnStop.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				if (registry != null)
-					try {
-						UnicastRemoteObject.unexportObject(registry, true);
-						System.out.println("Server stopped");
-					} catch (NoSuchObjectException e1) {
-						e1.printStackTrace();
-					}
-			}
-		});
-		btnStop.setBounds(239, 45, 85, 26);
-		btnStop.setText("Stop");
+		TableColumn tableColumn = new TableColumn(table, SWT.NONE);
+		tableColumn.setText("ID");
+		tableColumn.setResizable(false);
+		
+		TableColumn tableColumn_1 = new TableColumn(table, SWT.NONE);
+		tableColumn_1.setWidth(127);
+		tableColumn_1.setText("Date");
+		
+		TableColumn tableColumn_2 = new TableColumn(table, SWT.NONE);
+		tableColumn_2.setWidth(100);
+		tableColumn_2.setText("Time");
+		
+		TableColumn tableColumn_3 = new TableColumn(table, SWT.NONE);
+		tableColumn_3.setWidth(146);
+		tableColumn_3.setText("Jump Height ( km ) ");
+		
+		TableColumn tableColumn_4 = new TableColumn(table, SWT.NONE);
+		tableColumn_4.setWidth(138);
+		tableColumn_4.setText("Spaces available");
+		
+		Combo combo = new Combo(shell, SWT.NONE);
+		combo.setItems(new String[] {"Today", "This week", "Next week"});
+		combo.setBounds(43, 73, 176, 25);
+		combo.select(0);
+		
+		Button btnChangeSpaces = new Button(shell, SWT.NONE);
+		btnChangeSpaces.setBounds(38, 396, 134, 26);
+		btnChangeSpaces.setText("Change spaces");
 
 	}
 }
